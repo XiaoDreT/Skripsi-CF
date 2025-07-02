579d701e13d76be73e76d7440e085c18a41ff18f
JrHero14 <jeremi.herodian@besmart.id>
2025-07-02 09:21:12 +0700

    User responsive table

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -2,36 +2,48 @@
     <div class="col-md-12">
         <div class="card">
             <div class="card-body">
-                <a href="/admin/user/create" class="btn btn-primary mb-2"><i class="fas fa-plus"></i> Tambah</a>
+                <a href="/admin/user/create" class="btn btn-primary mb-2">
+                    <i class="fas fa-plus"></i> Tambah
+                </a>
 
-                <table class="table">
-                    <tr>
-                        <th>No</th>
-                        <th>Nama</th>
-                        <th>Email</th>
-                        <th>Role</th>
-                        <th>Action</th>
-                    </tr>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($user as $item)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->email }}</td>
+                                <td>{{ $item->role }}</td>
+                                <td>
+                                    <div class="d-flex">
+                                        <a href="/admin/user/{{ $item->id }}/edit" class="btn btn-info mr-2">
+                                            <i class="fas fa-edit"></i> Edit
+                                        </a>
+                                        <form action="/admin/user/{{ $item->id }}" method="POST">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button type="submit" class="btn btn-danger">
+                                                <i class="fas fa-trash"></i> Hapus
+                                            </button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
 
-                    @foreach ($user as $item)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $item->name }}</td>
-                        <td>{{ $item->email }}</td>
-                        <td>{{ $item->role }}</td>
-                        <td>
-                            <div class="d-flex">
-                                <a href="/admin/user/{{ $item->id }}/edit" class="btn btn-info mr-2"><i class="fas fa-edit">Edit</i></a>
-                                <form action="/admin/user/{{ $item->id }}" method="POST">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button type="submit" class="btn btn-danger"><i class="fas fa-trash">Hapus</i></button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                    @endforeach
-                </table>
             </div>
         </div>
     </div>
